b39fc695ed3f80edb70f510fc619bde0338d0dcd
Darielle Davis <hello@darielledavis.com>
2022-02-05 14:23:31 -0500

    creates register page template

--- app/public/wp-content/themes/matriarchy-build/functions.php
+++ app/public/wp-content/themes/matriarchy-build/functions.php
@@ -609,4 +609,41 @@ function redirection_function(){
         wp_safe_redirect( get_permalink( woocommerce_get_page_id( 'pro' ) ) );
     }
 }
+
+// separate registration page
+function redirect_login_page() {
+    
+	$register_page  = home_url( '/create-account' );
+	$login_page    = home_url( '/create-account?action=login' );
+	$page_viewed   = basename($_SERVER['REQUEST_URI']);
+   
+	if( $page_viewed == "wp-login.php" && $_SERVER['REQUEST_METHOD'] == 'GET') {
+	  wp_redirect($login_page);
+	  exit;
+	}
+	
+	if( $page_viewed == "wp-login.php?action=register" && $_SERVER['REQUEST_METHOD'] == 'GET') {
+	  wp_redirect($register_page);
+	  exit;
+	}
+  }
+  add_action('init','redirect_login_page');
+   
+  // Redirect For Login Failed
+  function login_failed() {
+	
+	wp_redirect( home_url( '/create-account?action=login&login=failed' ) );
+	exit;
+  }
+  add_action( 'wp_login_failed', 'login_failed' );
+   
+  // Redirect For Empty Username Or Password
+  function verify_username_password( $user, $username, $password ) {
+	if ( $username == "" || $password == "" ) {
+	  
+	  wp_redirect( home_url( '/create-account?action=login&login=empty' ) );
+	  exit;
+	}
+  }
+  add_filter( 'authenticate', 'verify_username_password', 1, 3);
 ?>
