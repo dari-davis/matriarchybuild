03dd71999491c85c84cfa7e5345d6df11e86dfd5
Darielle Davis <hello@darielledavis.com>
2022-02-05 17:10:33 -0500

    footer

--- app/public/wp-content/themes/matriarchy-build/footer.php
+++ app/public/wp-content/themes/matriarchy-build/footer.php
@@ -13,51 +13,47 @@
 				endif;
 			?>
 		</main><!-- /#main -->
-		<footer id="footer" class="footer">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-6">
-						<p><?php printf( esc_html__( '&copy; %1$s %2$s. All rights reserved.', 'matriarchy-build' ), date_i18n( 'Y' ), get_bloginfo( 'name', 'display' ) ); ?></p>
+		<footer id="footer" class="footer row no-gutters m-0">
+			<div class="footer__details col-md d-flex">
+				<div class="row no-gutters">
+					<div class="col-md-4 footer__logo py-4 py-md-0 text-center">
+						<a href="/" class="d-md-none"><img class="m-auto" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-footer-blue.svg"></a>
+						<a href="/" class="d-none d-md-block"><img class="m-auto" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-footer.svg"></a>
 					</div>
+					<div class="col-md-8 py-4">
+						<h3 class="text-white">Apply To Be A "Pro"</h3>
+						<p>Duis id volutpat urna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque lectus lorem, mollis ac purus vitae, aliquam finibus nunc.</p>
+						<a href="/" class="text-button">Apply Today</a>
+					</div>
+				</div>
+			</div>
+			<div class="footer__menu d-flex flex-column col-md px-4 py-3 py-md-5">
+				<?php
+					if ( has_nav_menu( 'footer-menu' ) ) : // See function register_nav_menus() in functions.php
+						/*
+							Loading WordPress Custom Menu (theme_location) ... remove <div> <ul> containers and show only <li> items!!!
+							Menu name taken from functions.php!!! ... register_nav_menu( 'footer-menu', 'Footer Menu' );
+							!!! IMPORTANT: After adding all pages to the menu, don't forget to assign this menu to the Footer menu of "Theme locations" /wp-admin/nav-menus.php (on left side) ... Otherwise the themes will not know, which menu to use!!!
+						*/
+						wp_nav_menu(
+							array(
+								'theme_location'  => 'footer-menu',
+								'container'       => 'nav',
+								'container_class' => 'd-flex',
+								'fallback_cb'     => '',
+								'items_wrap'      => '<ul class="menu nav flex-column">%3$s</ul>',
+								//'fallback_cb'    => 'WP_Bootstrap4_Navwalker_Footer::fallback',
+								'walker'          => new WP_Bootstrap4_Navwalker_Footer(),
+							)
+						);
+					endif;
 
-					<?php
-						if ( has_nav_menu( 'footer-menu' ) ) : // See function register_nav_menus() in functions.php
-							/*
-								Loading WordPress Custom Menu (theme_location) ... remove <div> <ul> containers and show only <li> items!!!
-								Menu name taken from functions.php!!! ... register_nav_menu( 'footer-menu', 'Footer Menu' );
-								!!! IMPORTANT: After adding all pages to the menu, don't forget to assign this menu to the Footer menu of "Theme locations" /wp-admin/nav-menus.php (on left side) ... Otherwise the themes will not know, which menu to use!!!
-							*/
-							wp_nav_menu(
-								array(
-									'theme_location'  => 'footer-menu',
-									'container'       => 'nav',
-									'container_class' => 'col-md-6',
-									'fallback_cb'     => '',
-									'items_wrap'      => '<ul class="menu nav justify-content-end">%3$s</ul>',
-									//'fallback_cb'    => 'WP_Bootstrap4_Navwalker_Footer::fallback',
-									'walker'          => new WP_Bootstrap4_Navwalker_Footer(),
-								)
-							);
-						endif;
-
-						if ( is_active_sidebar( 'third_widget_area' ) ) :
-					?>
-						<div class="col-md-12">
-							<?php
-								dynamic_sidebar( 'third_widget_area' );
-
-								if ( current_user_can( 'manage_options' ) ) :
-							?>
-								<span class="edit-link"><a href="<?php echo esc_url( admin_url( 'widgets.php' ) ); ?>" class="badge badge-secondary"><?php esc_html_e( 'Edit', 'matriarchy-build' ); ?></a></span><!-- Show Edit Widget link -->
-							<?php
-								endif;
-							?>
-						</div>
-					<?php
-						endif;
-					?>
-				</div><!-- /.row -->
-			</div><!-- /.container -->
+				?>
+				<ul class="footer__social-menu d-flex flex-row p-0 m-0">
+					<li class="footer__social-menu-item"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/twitter.svg"></a></li>
+					<li class="footer__social-menu-item"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/facebook.svg"></a></li>
+				</ul>
+			</div>
 		</footer><!-- /#footer -->
 	</div><!-- /#wrapper -->
 	<?php
