f8da04d430c89a041ab1b337871c05bb59e2737f
Darielle Davis <hello@darielledavis.com>
2022-02-16 18:36:44 -0500

    styles full page template

--- app/public/wp-content/themes/matriarchy-build/page-full.php
+++ app/public/wp-content/themes/matriarchy-build/page-full.php
@@ -9,22 +9,34 @@ get_header();
 
 the_post();
 ?>
-<div id="post-<?php the_ID(); ?>" <?php post_class( 'content' ); ?>>
-	<h1 class="entry-title visually-hidden"><?php the_title(); ?></h1>
-	<?php
-		the_content();
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . __( 'Pages:', 'matriarchy-build' ),
-			'after'  => '</div>',
-		) );
-		edit_post_link( __( 'Edit', 'matriarchy-build' ), '<span class="d-none edit-link">', '</span>' );
-	?>
-</div><!-- /#post-<?php the_ID(); ?> -->
+<div class="row no-gutters m-0 position-relative">
+	<div class="col-12 p-0">
+		<?php if (has_post_thumbnail()): ?>
+			<a class="page__image-wrapper" href="<?php the_permalink(); ?>"><?php the_post_thumbnail('full', ['class' => 'page__featured-image']); ?></a>
+		<?php else: ?>
+			<div class="page__featured-image"></div>
+		<?php endif; ?>
+
+		<div id="post-<?php the_ID(); ?>" <?php post_class( 'page__content-wrapper d-flex' ); ?>>
+			<h1 class="page__title entry-title mb-borders mb-borders--pink pt-4 pb-3 mb-0 text-center"><?php the_title(); ?></h1>
+
+			<div class="page__content mb-borders mb-borders--pink">
+				<div class="page__content-inner mx-auto">
+					<?php
+						the_content();
+
+						wp_link_pages( array(
+							'before' => '<div class="page-links">' . __( 'Pages:', 'matriarchy-build' ),
+							'after'  => '</div>',
+						) );
+						edit_post_link( __( 'Edit', 'matriarchy-build' ), '<span class="d-none edit-link">', '</span>' );
+					?>
+				</div>
+			</div>
+		</div><!-- /#post-<?php the_ID(); ?> -->
+	</div>
+</div>
 <?php
-	// If comments are open or we have at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) :
-		comments_template();
-	endif;
 
 get_footer();
