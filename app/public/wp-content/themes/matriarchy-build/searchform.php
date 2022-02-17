b6ec799b7dac3faa93e8796ce7e58081e84d9cb9
Darielle Davis <hello@darielledavis.com>
2022-02-17 08:50:56 -0500

    styles no search restults view

--- app/public/wp-content/themes/matriarchy-build/searchform.php
+++ app/public/wp-content/themes/matriarchy-build/searchform.php
@@ -3,9 +3,11 @@
  * The template for displaying search forms.
  */
 ?>
-<form class="search-form" role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<div class="input-group">
-		<input type="text" name="s" class="form-control" placeholder="<?php esc_attr_e( 'Search', 'matriarchy-build' ); ?>" />
-		<button type="submit" class="btn btn-secondary" name="submit"><?php esc_html_e( 'Search', 'matriarchy-build' ); ?></button>
-	</div><!-- /.input-group -->
+<div class="col-md-8">
+	<form class="search-form" role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+		<div class="input-group">
+			<input type="text" name="s" class="form-control" placeholder="<?php esc_attr_e( 'Search', 'matriarchy-build' ); ?>" />
+			<button type="submit" class="btn btn-secondary" name="submit"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/search.svg"/></button>
+		</div><!-- /.input-group -->
+	</div>
 </form>
