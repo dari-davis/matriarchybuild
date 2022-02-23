0dc13dab862ee8bbd0cf2772337e0fda191e1c58
Darielle Davis <hello@darielledavis.com>
2022-02-23 10:50:12 -0500

    miscellaneous updates

--- app/public/wp-content/themes/matriarchy-build/includes/taxonomies.php
+++ app/public/wp-content/themes/matriarchy-build/includes/taxonomies.php
@@ -5,19 +5,19 @@
 add_action('init', function() {
   register_taxonomy('pros', ['pro'], array(
     'labels' => array(
-      'name' => __('Pro Category'),
-      'sinular_name' => __('Pro Category'),
-      'search_items' => __('Serach Pro Categories'),
-      'all_items' => __('All Pro Categories'),
-      'parent_item' => __('Parent Pro Categories'),
-      'parent_item_colon' => __('Parent Pro Categories:'),
-      'edit_item' => __('Edit Pro Category'),
-      'update_item' => __('Update Pro Category'),
-      'add_new_item' => __('Add New Pro Category'),
-      'new_item_name' => __('New Pro Category Name'),
-      'menu_name' => __('Pro Categories'),
-      'not_found' => __('No Pro Categories found.'),
-      'not_found_in_trash' => __('No Pro Categories found in Trash.')
+      'name' => __('Pro Trade'),
+      'sinular_name' => __('Pro Trade'),
+      'search_items' => __('Serach Pro Trades'),
+      'all_items' => __('All Pro Trades'),
+      'parent_item' => __('Parent Pro Trades'),
+      'parent_item_colon' => __('Parent Pro Trades:'),
+      'edit_item' => __('Edit Pro Trade'),
+      'update_item' => __('Update Pro Trade'),
+      'add_new_item' => __('Add New Pro Trade'),
+      'new_item_name' => __('New Pro Trade Name'),
+      'menu_name' => __('Pro Trades'),
+      'not_found' => __('No Pro Trades found.'),
+      'not_found_in_trash' => __('No Pro Trades found in Trash.')
     ),
     'hierarchial' => true,
     'show_in_rest' => true
