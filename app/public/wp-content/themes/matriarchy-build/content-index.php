258b5ac1fe20d974ed98c3805f2f4c024c711496
Darielle Davis <hello@darielledavis.com>
2022-03-08 12:45:15 -0500

    adds meta data to posts loop

--- app/public/wp-content/themes/matriarchy-build/content-index.php
+++ app/public/wp-content/themes/matriarchy-build/content-index.php
@@ -18,5 +18,20 @@
 			<?php endif; ?>
 			<a class="archives__name" href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'matriarchy-build' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><div class="archives__title p-2"><?php the_title(); ?></div></a>
 		</div>
+		<?php if ( 'post' === get_post_type() ): ?>
+			<div class="entry-meta m-0">
+				<div class="row m-0">
+					<div class="single-post__date col-auto p-0 d-flex">
+						<span><?= the_time('m'); ?></span>
+						<span><?= the_time('j'); ?></span>
+						<span><?= the_time('y'); ?></span>
+					</div>
+					<div class="single-post__meta col-auto p-0 d-flex flex-column">
+						<span class="px-2">by <?= get_the_author(); ?></span>
+						<span class="px-2"><?= the_category(' , '); ?></span>
+					</div>
+				</div>
+			</div><!-- /.entry-meta -->
+		<?php endif; ?>
 	<?php endif; ?>
 </article><!-- /#post-<?php the_ID(); ?> -->
