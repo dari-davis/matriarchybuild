11fce03fdddd7e453f2aa95ec915c6559f393377
Darielle Davis <hello@darielledavis.com>
2022-03-10 06:35:29 -0500

    creates content blocks page

---
+++ app/public/wp-content/themes/matriarchy-build/page-content-blocks.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: Page (Default)
+ * Description: Page template with Sidebar on the left side.
+ *
+ */
+
+get_header();
+?>
+
+<div class="main__row row no-gutters m-0">
+	<div class="col-12 p-0 content-blocks mx-auto">
+        <div class="content-blocks__section my-5">
+            <div>Content Blocks.</div>
+        </div>
+    </div>
+</div>
+
+<?php
+
+get_footer();
