64fc7985a88b16ae6d5d869102bea30e9a8cdee1
Darielle Davis <hello@darielledavis.com>
2022-03-10 07:08:12 -0500

    creates article component

--- app/public/wp-content/themes/matriarchy-build/content-index.php
+++ app/public/wp-content/themes/matriarchy-build/content-index.php
@@ -10,28 +10,6 @@
 		<div class="archives__pro mb-5">
 		<?= get_template_part('partials/pro-card'); ?></div>
 	<?php else: ?>
-		<div class="archives__details col p-0 mb-borders">
-			<?php if (has_post_thumbnail()): ?>
-				<a class="archives__image-container" href="<?php the_permalink(); ?>" style="background-image: url('<?= get_the_post_thumbnail_url(); ?>');"></a>
-			<?php else: ?>
-				<a class="archives__image-container" href="<?php the_permalink(); ?>"><div class="archives__image"></div></a>
-			<?php endif; ?>
-			<a class="archives__name" href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'matriarchy-build' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><div class="archives__title p-2"><?php the_title(); ?></div></a>
-		</div>
-		<?php if ( 'post' === get_post_type() ): ?>
-			<div class="entry-meta m-0">
-				<div class="row m-0">
-					<div class="single-post__date col-auto p-0 d-flex">
-						<span><?= the_time('m'); ?></span>
-						<span><?= the_time('j'); ?></span>
-						<span><?= the_time('y'); ?></span>
-					</div>
-					<div class="single-post__meta col-auto p-0 d-flex flex-column">
-						<span class="px-2">by <?= get_the_author(); ?></span>
-						<span class="px-2"><?= the_category(' , '); ?></span>
-					</div>
-				</div>
-			</div><!-- /.entry-meta -->
-		<?php endif; ?>
+		<?= get_template_part('partials/article'); ?>
 	<?php endif; ?>
 </article><!-- /#post-<?php the_ID(); ?> -->
---
+++ app/public/wp-content/themes/matriarchy-build/partials/article.php
@@ -0,0 +1,25 @@
+<div class="article">
+    <div class="article__details col p-0 mb-borders">
+        <?php if (has_post_thumbnail()): ?>
+            <a class="article__image-container" href="<?php the_permalink(); ?>" style="background-image: url('<?= get_the_post_thumbnail_url(); ?>');"></a>
+        <?php else: ?>
+            <a class="article__image-container" href="<?php the_permalink(); ?>"><div class="article__image"></div></a>
+        <?php endif; ?>
+        <a class="article__name" href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'matriarchy-build' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><div class="article__title p-2"><?php the_title(); ?></div></a>
+    </div>
+    <?php if ( 'post' === get_post_type() ): ?>
+        <div class="entry-meta m-0">
+            <div class="row m-0">
+                <div class="article__date col-auto p-0 d-flex">
+                    <span><?= the_time('m'); ?></span>
+                    <span><?= the_time('j'); ?></span>
+                    <span><?= the_time('y'); ?></span>
+                </div>
+                <div class="article__meta col-auto px-0 d-flex flex-column">
+                    <span class="px-2">by <?= get_the_author(); ?></span>
+                    <span class="px-2"><?= the_category(' , '); ?></span>
+                </div>
+            </div>
+        </div><!-- /.entry-meta -->
+    <?php endif; ?>
+</div>
