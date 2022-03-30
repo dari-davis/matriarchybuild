1c08a672de49685307356c484b647925340b463c
Darielle Davis <hello@darielledavis.com>
2022-03-29 21:39:50 -0400

    updates How It Works to use block data

--- app/public/wp-content/themes/matriarchy-build/templates/blocks/how-it-works.php
+++ app/public/wp-content/themes/matriarchy-build/templates/blocks/how-it-works.php
@@ -7,7 +7,7 @@ $page = is_page('How It Works');
 
 ?>
 
-<div class="how-it-works <?php if (!$page): ?>content-block <?php endif; ?>d-lg-flex flex-column">
+<div class="how-it-works d-lg-flex flex-column<?php if (!$page): ?> content-block" style="background-color: <?= get_field('background_color'); ?>;<?php endif; ?>">
     <div class="how-it-works__container <?php if ($page): ?>d-md-flex flex-column flex-lg-row<?php else: ?>d-lg-flex<?php endif; ?>">
         <div class="how-it-works__header row col-11 col-md-5 col-lg-4 gx-0">
             <div class="how-it-works__scafolding col-3 col-lg-6"></div>
@@ -18,38 +18,26 @@ $page = is_page('How It Works');
             </div>
         </div>
         <div class="how-it-works__content how-it-works__slides row gx-0">
-            <div class="how-it-works__section mb-borders <?php if ($page): ?>d-flex<?php endif; ?>">
-                <div class="how-it-works__top <?php if ($page): ?>d-md-flex col-md-6<?php else: ?>d-lg-flex<?php endif; ?>">
-                    <div class="how-it-works__step">Step 1 <img class="d-none <?php if ($page): ?>d-md-inline<?php else: ?>d-lg-inline<?php endif; ?>" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/arrow_blue_medium.svg" alt="blue arrow"/></div>
-                    <div class="how-it-works__title h2 mb-0">
-                        <span>Search</span>
-                        <div class="how-it-works__scafolding d-none <?php if ($page): ?>d-md-block<?php endif;?> col-1"></div>  
+            <?php if (have_rows('steps')): ?>
+                <?php while (have_rows('steps')): the_row(); ?>
+                    <div class="how-it-works__section mb-borders <?php if ($page): ?>d-flex<?php endif; ?>" style="background-color: <?= get_field('active_step_background_color'); ?>;">
+                        <div class="how-it-works__top <?php if ($page): ?>d-md-flex col-md-6<?php else: ?>d-lg-flex<?php endif; ?>">
+                            <div class="how-it-works__step">Step <?= get_row_index(); ?><img class="d-none <?php if ($page): ?>d-md-inline<?php else: ?>d-lg-inline<?php endif; ?>" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/arrow_blue_medium.svg" alt="blue arrow"/></div>
+                            <div class="how-it-works__title h2 mb-0">
+                                <span><?= get_sub_field('title'); ?></span>
+                                <div class="how-it-works__scafolding d-none <?php if ($page): ?>d-md-block<?php endif;?> col-1"></div>  
+                            </div>
+                        </div>
+                        <div class="how-it-works__description <?php if ($page): ?>flex-column<?php endif; ?>">
+                            <span><?= get_sub_field('description'); ?></span>
+                            <?php if (get_sub_field('blurb')): ?>
+                                <?php if ($page): ?><p class="how-it-works__blurb"><?= get_sub_field('blurb'); ?></p><?php endif; ?>
+                            <?php endif; ?>
+                            <div class="how-it-works__scafolding d-none <?php if (!$page): ?>d-lg-block<?php endif;?> col-1"></div>
+                        </div>
                     </div>
-                </div>
-                <div class="how-it-works__description <?php if ($page): ?>flex-column<?php endif; ?>">
-                    <span>Search our database of Pros based on your project type.</span>
-                    <?php if ($page): ?>
-                        <p class="how-it-works__blurb">Curabitur commodo molestie ante. Integer sagittis felis ullamcorper velit euismod, et vulputate magna viverra. Mauris a cursus dolor. Suspendisse suscipit molestie massa, vitae aliquet nisi hendrerit ornare. Nulla tempor posuere augue, nec faucibus orci faucibus pharetra.</p>
-                    <?php endif; ?>
-                    <div class="how-it-works__scafolding d-none <?php if (!$page): ?>d-lg-block<?php endif;?> col-1"></div>
-                </div>
-            </div>
-            <div class="how-it-works__section mb-borders <?php if ($page): ?>d-flex<?php endif; ?>">
-                <div class="how-it-works__top <?php if ($page): ?>d-md-flex col-md-6<?php else: ?>d-lg-flex<?php endif; ?>">
-                    <div class="how-it-works__step">Step 1 <img class="d-none <?php if ($page): ?>d-md-inline<?php else: ?>d-lg-inline<?php endif; ?>" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/arrow_blue_medium.svg" alt="blue arrow"/></div>
-                    <div class="how-it-works__title h2 mb-0">
-                        <span>Build</span>
-                        <div class="how-it-works__scafolding d-none <?php if ($page): ?>d-md-block<?php endif;?> col-1"></div>  
-                    </div>
-                </div>
-                <div class="how-it-works__description <?php if ($page): ?>flex-column<?php endif; ?>">
-                    <span>Search our database of Pros based on your project type.</span>
-                    <?php if ($page): ?>
-                        <p class="how-it-works__blurb">Curabitur commodo molestie ante. Integer sagittis felis ullamcorper velit euismod, et vulputate magna viverra. Mauris a cursus dolor. Suspendisse suscipit molestie massa, vitae aliquet nisi hendrerit ornare. Nulla tempor posuere augue, nec faucibus orci faucibus pharetra.</p>
-                    <?php endif; ?>
-                    <div class="how-it-works__scafolding d-none <?php if (!$page): ?>d-lg-block<?php endif;?> col-1"></div>
-                </div>
-            </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
         </div>
     </div>
 </div>
