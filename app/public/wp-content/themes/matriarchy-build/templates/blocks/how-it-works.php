36cf5dec100caca1b3b74e51e6ae29cf2d57c2ef
Darielle Davis <hello@darielledavis.com>
2022-03-30 09:02:40 -0400

    changes how it works to support archive page

--- app/public/wp-content/themes/matriarchy-build/templates/blocks/how-it-works.php
+++ app/public/wp-content/themes/matriarchy-build/templates/blocks/how-it-works.php
@@ -3,14 +3,15 @@
  * Block: How It Works
  */
 
-$page = is_page('How It Works');
-
+$page = is_page('how-it-works');
+$howItWorksPage = get_page_by_path('how-it-works')->ID;
+if (is_archive('pro')) { $backgroundColor = get_field('background_color', $howItWorksPage); } else { $backgroundColor = get_field('background_color'); }
 ?>
 
-<div class="how-it-works d-lg-flex flex-column<?php if (!$page): ?> content-block" style="background-color: <?= get_field('background_color'); ?>;<?php endif; ?>">
+<div class="how-it-works <?php if ($page): ?>how-it-works--page <?php endif; ?>d-lg-flex flex-column<?php if (!$page): ?> content-block" style="background-color: <?= $backgroundColor; ?>;<?php endif; ?>">
     <div class="how-it-works__container <?php if ($page): ?>d-md-flex flex-column flex-lg-row<?php else: ?>d-lg-flex<?php endif; ?>">
         <div class="how-it-works__header row col-11 col-md-5 col-lg-4 gx-0">
-            <div class="how-it-works__scafolding col-3 col-lg-6"></div>
+            <div class="how-it-works__scafolding col-3 col-lg-6" data-no-lazy></div>
             <div class="how-it-works__heading col-6 mb-borders d-flex justify-content-center align-items-center"><h2 class="m-0">How It Works</h2></div>
             <div class="how-it-works__arrows col-2 d-flex flex-column flex-lg-row<?php if($page): ?> d-none<?php endif; ?>">
                 <button class="how-it-works__prev prev" aria-label="Previous" type="button"></button>
@@ -18,14 +19,14 @@ $page = is_page('How It Works');
             </div>
         </div>
         <div class="how-it-works__content how-it-works__slides row gx-0">
-            <?php if (have_rows('steps')): ?>
-                <?php while (have_rows('steps')): the_row(); ?>
+            <?php if (have_rows('steps', $howItWorksPage)): ?>
+                <?php while (have_rows('steps', $howItWorksPage)): the_row(); ?>
                     <div class="how-it-works__section mb-borders <?php if ($page): ?>d-flex<?php endif; ?>" style="background-color: <?= get_field('active_step_background_color'); ?>;">
                         <div class="how-it-works__top <?php if ($page): ?>d-md-flex col-md-6<?php else: ?>d-lg-flex<?php endif; ?>">
-                            <div class="how-it-works__step">Step <?= get_row_index(); ?><img class="d-none <?php if ($page): ?>d-md-inline<?php else: ?>d-lg-inline<?php endif; ?>" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/arrow_blue_medium.svg" alt="blue arrow"/></div>
+                            <div class="how-it-works__step">Step <?= get_row_index(); ?><img data-no-lazy class="d-none <?php if ($page): ?>d-md-inline<?php else: ?>d-lg-inline<?php endif; ?>" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/arrow_blue_medium.svg" alt="blue arrow"/></div>
                             <div class="how-it-works__title h2 mb-0">
                                 <span><?= get_sub_field('title'); ?></span>
-                                <div class="how-it-works__scafolding d-none <?php if ($page): ?>d-md-block<?php endif;?> col-1"></div>  
+                                <div class="how-it-works__scafolding d-none <?php if ($page): ?>d-md-block<?php endif;?> col-1" data-no-lazy></div>  
                             </div>
                         </div>
                         <div class="how-it-works__description <?php if ($page): ?>flex-column<?php endif; ?>">
@@ -33,7 +34,7 @@ $page = is_page('How It Works');
                             <?php if (get_sub_field('blurb')): ?>
                                 <?php if ($page): ?><p class="how-it-works__blurb"><?= get_sub_field('blurb'); ?></p><?php endif; ?>
                             <?php endif; ?>
-                            <div class="how-it-works__scafolding d-none <?php if (!$page): ?>d-lg-block<?php endif;?> col-1"></div>
+                            <div class="how-it-works__scafolding d-none <?php if (!$page): ?>d-lg-block<?php endif;?> col-1" data-no-lazy></div>
                         </div>
                     </div>
                 <?php endwhile; ?>
