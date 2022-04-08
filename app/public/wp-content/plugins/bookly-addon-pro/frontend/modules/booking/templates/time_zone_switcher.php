92a8fabd2950243441120c6ab63f0e0704ea1687
Darielle Davis <hello@darielledavis.com>
2022-04-08 00:07:26 -0400

    updates timezone switcher

--- app/public/wp-content/plugins/bookly-addon-pro/frontend/modules/booking/templates/time_zone_switcher.php
+++ app/public/wp-content/plugins/bookly-addon-pro/frontend/modules/booking/templates/time_zone_switcher.php
@@ -1,4 +1,7 @@
 <?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
-<select disabled class="bookly-js-time-zone-switcher bookly-time-zone-switcher">
-    <?php echo $time_zone_options ?>
-</select>
+<div class="bookly-js-time-zone-switcher d-flex align-items-center">
+    <span class="bookly-form__timezone-label me-2">Timezone</span>
+    <select disabled class="bookly-time-zone-switcher">
+        <?php echo $time_zone_options ?>
+    </select>
+</div>
