63cbc7538d044bb0bb34e624e6ed56e5229c6671
Darielle Davis <hello@darielledavis.com>
2022-04-19 14:38:37 -0400

    allows editors to see, add and update users

--- app/public/wp-content/themes/matriarchy-build/functions.php
+++ app/public/wp-content/themes/matriarchy-build/functions.php
@@ -1105,8 +1105,42 @@ function klf_acf_input_admin_footer() { ?>
 	});
 	})(jQuery);
 	</script>
-	<?php }
-	add_action('acf/input/admin_footer', 'klf_acf_input_admin_footer');
+<?php }
+add_action('acf/input/admin_footer', 'klf_acf_input_admin_footer');
 
+// lets Editors manage users
+function editor_manage_users() {
+	if (get_option('add_cap_editor_once') != 'done') {
+		$edit_editor = get_role('editor');
+		$edit_editor->add_cap('edit_users');
+		$edit_editor->add_cap('list_users');
+		$edit_editor->add_cap('promote_users');
+		$edit_editor->add_cap('create_users');
+		$edit_editor->add_cap('add_users');
+		$edit_editor->add_cap('delete_users');
+
+		update_option('add_cap_editor_once', 'done');
+	}
+}
+add_action('init', 'editor_manage_users');
+
+// hide administrators from user list
+add_action('pre_user_query', 'isa_pre_user_query');
+function isa_pre_user_query($user_search) {
+	$user = wp_get_current_user();
+
+	if (!current_user_can('manage_options')) {
+		global $wpdb;
+
+		$user_search->query_where =
+			str_replace('WHERE 1=1',
+			"WHERE 1=1 AND {$wpdb->users}.ID IN (
+				SELECT {$wpdb->usermeta}.user_id FROM $wpdb->usermeta
+					WHERE {$wpdb->usermeta}.meta_key = '{$wpdb->prefix}capabilities'
+					AND {$wpdb->usermeta}.meta_value NOT LIKE '%administrator%')",
+			$user_search->query_where
+		);
+	}
+}
 
 ?>
