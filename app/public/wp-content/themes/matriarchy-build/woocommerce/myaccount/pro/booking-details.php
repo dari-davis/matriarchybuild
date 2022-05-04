90338ac060ea55e001951b73602ed4a95a28075a
Darielle Davis <hello@darielledavis.com>
2022-05-04 11:51:16 -0400

    fixes image paths and updates Pros photos view

--- app/public/wp-content/themes/matriarchy-build/woocommerce/myaccount/pro/booking-details.php
+++ app/public/wp-content/themes/matriarchy-build/woocommerce/myaccount/pro/booking-details.php
@@ -183,8 +183,8 @@ $upload_dir = wp_upload_dir();
                 <?php foreach(array_slice($photos, 0, 5) as $photo): ?>
                     <?php $entry = $wpdb->get_results('SELECT * FROM wp_frmt_form_entry_meta WHERE entry_id="'.$photo->entry_id.'"');
                             $src = $wpdb->get_results('SELECT meta_value FROM wp_frmt_form_entry_meta WHERE meta_key="upload-1" AND entry_id="'.$photo->entry_id.'"');
-                            $url = explode("ugc/$user_id/", $src[0]->meta_value);
-                            $image = $upload_dir['url'] . "/ugc/$user_id/" . str_replace('";}}', '', $url[2]);
+                            $path = explode("/uploads/", $src[0]->meta_value)[2];
+                            $image - $upload_dir['baseurl'] . "/" . str_replace('";}}', '', $path);
                             $attachment = attachment_url_to_postid($image); ?>
                     <div class="questionnaire__image me-3">
                         <a class="questionnaire__image-link d-block" href="#" data-slick-index="<?= $imageIndex; ?>">
@@ -207,11 +207,11 @@ $upload_dir = wp_upload_dir();
     <div class="photos__dialog p-0 mx-md-auto" id="dialog">
         <?php foreach($photos as $photo): ?>
             <?php $src = $wpdb->get_results('SELECT meta_value FROM wp_frmt_form_entry_meta WHERE meta_key="upload-1" AND entry_id="'.$photo->entry_id.'"');
-            $url = explode("ugc/$user_id/", $src[0]->meta_value); ?>
+            $path = explode("/uploads/", $src[0]->meta_value)[2]; ?>
             <div class="photos__image d-flex justify-content-center align-items-center">
                 <div class="photos__image-container">
                     <div class="image-inner">
-                        <img data-no-lazy src="<?= $upload_dir['url'] . "/ugc/$user_id/" . str_replace('";}}', '', $url[2]); ?>"/>
+                        <img data-no-lazy src="<?= $upload_dir['baseurl'] . "/" . str_replace('";}}', '', $path); ?>"/>
                     </div>
                 </div>
             </div>    
