1cbb25a1d9424f7af8b2cfe1460837987e44a6d7
Darielle Davis <hello@darielledavis.com>
2022-05-09 10:29:28 -0400

    removes twitter and facebook from the footer

--- app/public/wp-content/themes/matriarchy-build/footer.php
+++ app/public/wp-content/themes/matriarchy-build/footer.php
@@ -71,9 +71,7 @@
 			<ul class="footer__social-menu d-flex flex-row p-0 m-0 pt-3 pt-md-5">
 				<li class="footer__social-menu-item"><a href="https://www.instagram.com/matriarchybuild/" target="_blank"><img height="22" width="22" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/footer/instagram.svg" alt="Instagram icon for Matriarchy Build footer"></a></li>
 				<li class="footer__social-menu-item"><a href="https://www.tiktok.com/@matriarchybuild" target="_blank"><img height="22" width="22" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/footer/tiktok.svg" alt="TikTok icon for Matriarchy Build footer"></a></li>
-				<li class="footer__social-menu-item"><a href="https://www.facebook.com/Matriarchy-Build-100699602430280" target="_blank"><img height="22" width="22" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/footer/facebook.svg" alt="Facebook icon for Matriarchy Build footer"></a></li>
 				<li class="footer__social-menu-item"><a href="https://www.pinterest.com/04dexx5gswbh2v4on157ftn5y7pzq9/_saved/" target="_blank"><img height="22" width="22" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/footer/pinterest.svg" alt="Pinterest icon for Matriarchy Build footer"></a></li>
-				<li class="footer__social-menu-item"><a href="https://twitter.com/MatriarchyBuild" target="_blank"><img height="22" width="22" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/footer/twitter.svg" alt="Twitter icon for Matriarchy Build footer"></a></li>
 				<li class="footer__social-menu-item"><a href="https://www.youtube.com/channel/UCVIsS1vBde3-csSrrLrJQ9Q" target="_blank"><img height="22" width="22" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/footer/youtube.svg" alt="YouTube icon for Matriarchy Build footer"></a></li>
 			</ul>
 		</div>
