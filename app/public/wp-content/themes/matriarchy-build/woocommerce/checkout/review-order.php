481251a91ed53d5b9ab12fe1bae91bd2697b75c5
dari-davis <hello@darielledavis.com>
2022-09-09 07:36:02 -0400

    Revert "fixes duplicate listings in checkout"
    This reverts commit 1e4a4e3babbe77233d8271b52c6fca180d56a20f.

--- app/public/wp-content/themes/matriarchy-build/woocommerce/checkout/review-order.php
+++ app/public/wp-content/themes/matriarchy-build/woocommerce/checkout/review-order.php
@@ -20,28 +20,28 @@ defined( 'ABSPATH' ) || exit;
 global $wpdb;
 
 // gets all the cart data
-foreach ( WC()->cart->get_cart() as $wc_key => $cart_item ) {
-	$_product = wc_get_product( $cart_item['product_id'] );
+foreach ( WC()->cart->get_cart() as $wc_key => $wc_item ) {
+	$_product = wc_get_product( $wc_item['product_id'] );
 	$is_consultation = $_product->is_type('simple') && strpos($_product->name, 'Consultation') != false;
 
 	if ($is_consultation) {
-		$timezone = $cart_item['bookly']['time_zone'];
+		$timezone = $wc_item['bookly']['time_zone'];
 
 		// staff details
-		$staffId = $cart_item['bookly']['items'][0]['staff_ids'][0];
+		$staffId = $wc_item['bookly']['items'][0]['staff_ids'][0];
 		$staffInfo = $wpdb->get_results('SELECT * FROM wp_bookly_staff WHERE id="'.$staffId.'";');
 		$staffName = $staffInfo[0]->full_name;
 		$staffSlug = str_replace(" ", "-", strtolower($staffName));
 		$staffTrade = get_post_meta(get_page_by_path($staffSlug, OBJECT, 'pro')->ID, 'trade', true);
 
 		// service details
-		$serviceId = $cart_item['bookly']['items'][0]['service_id'];
+		$serviceId = $wc_item['bookly']['items'][0]['service_id'];
 		$serviceInfo = $wpdb->get_results('SELECT * FROM wp_bookly_services WHERE id="'.$serviceId.'";');
 
 		// appointment time and date
-		//var_dump($cart_item['bookly']['items'][0]);
-		$unixTime = strtotime($cart_item['bookly']['items'][0]['slots'][0][2]);
-		//var_dump($cart_item['bookly']['items'][0]['slots'][0][2]);
+		//var_dump($wc_item['bookly']['items'][0]);
+		$unixTime = strtotime($wc_item['bookly']['items'][0]['slots'][0][2]);
+		//var_dump($wc_item['bookly']['items'][0]['slots'][0][2]);
 		$date = date('M j, Y', $unixTime);
 		$UTCTime = date('g:i a', $unixTime);
 
@@ -56,81 +56,90 @@ foreach ( WC()->cart->get_cart() as $wc_key => $cart_item ) {
 		$endTime->add(new DateInterval('PT' . $duration . 'M'));
 		$timeOfDay = date_format($endTime, 'a');
 	}
-
-	if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_checkout_cart_item_visible', true, $cart_item) ) { ?>
-
-		<?php if ($is_consultation): ?>
-			<div class="consultation-card row mb-borders m-0 mb-4">
-				<div class="col-12 col-lg bg-white p-3">
-					<div class="consultation-card__detail mb-2"><?= $serviceInfo[0]->title;?></div>
-					<div class="consultation-card__pro"><?= $staffName; ?></div>
-					<div class="consultation-card__detail"><?= $staffTrade; ?></div>
-				</div>
-				<div class="col-12 col-lg p-3">
-					<div><?= $date; ?></div>
-					<div><?= date_format($startTime, 'g:i').'-'.date_format($endTime, 'g:i').$timeOfDay; ?></div>
-					<div><a class="text-button text-button--green" href="/pro/<?= str_replace(' ', '-', strtolower($staffInfo[0]->full_name)); ?>/#booking-<?= $serviceId; ?>">Change Time</a></div>
-				</div>
-				<div class="col-12 col-lg p-3 d-flex justify-content-lg-end">
-					<?= WC()->cart->get_product_subtotal($_product, $cart_item['quantity']); ?>
-				</div>
-			</div>
-		<?php else: ?>
-			<div class="order-item row mb-borders m-0 mb-4">
-				<div class="col-12 col-lg p-3">
-					<div class="order-item__image"><?= $_product->get_image(); ?></div>
-				</div>
-				<div class="col-12 col-lg p-3">
-					<div class="order-item__name product-name mb-3" data-title="<?php esc_attr_e( 'Product', 'woocommerce' ); ?>">
-						<?php
-						if ( ! $product_permalink ) {
-							echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) . '&nbsp;' );
-						} else {
-							echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', sprintf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $_product->get_name() ), $cart_item, $cart_item_key ) );
-						}
-
-						do_action( 'woocommerce_after_cart_item_name', $cart_item, $cart_item_key ); ?>
-					</div>
-
-					<div class="order-item__details">
-						<?= wc_get_formatted_cart_item_data($cart_item); // PHPCS: XSS ok. ?>
-					</div>
-
-					<div class="product-price mb-3" data-title="<?php esc_attr_e( 'Price', 'woocommerce' ); ?>">
-						<?php
-							echo apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key ); // PHPCS: XSS ok.
-						?>
-					</div>
-
-					<div class="product-remove">
-						<?php
-							echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-								'woocommerce_cart_item_remove_link',
-								sprintf(
-									'<a href="%s" class="text-button text-button--green" aria-label="%s" data-product_id="%s" data-product_sku="%s">Remove</a>',
-									esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
-									esc_html__( 'Remove this item', 'woocommerce' ),
-									esc_attr( $product_id ),
-									esc_attr( $_product->get_sku() )
-								),
-								$cart_item_key
-							);
-						?>
-					</div>
-				</div>
-
-				<div class="col-12 col-lg p-3 d-flex justify-content-lg-end">
-					<div class="product-subtotal" data-title="<?php esc_attr_e( 'Subtotal', 'woocommerce' ); ?>">
-						<?php echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); // PHPCS: XSS ok. ?>
-					</div>
-				</div>
-			</div>
-		<?php endif; ?>
-		<?php
-	}
 }
+?>
 
-do_action( 'woocommerce_review_order_after_cart_contents' );
+<?php
+	do_action( 'woocommerce_review_order_before_cart_contents' );
+
+	foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {		
+		$_product = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
+		$is_consultation = $_product->is_type('simple') && strpos($_product->name, 'Consultation') != false;
+
+		if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_checkout_cart_item_visible', true, $cart_item, $cart_item_key ) ) { ?>
+
+			<?php if ($is_consultation): ?>
+				<div class="consultation-card row mb-borders m-0 mb-4">
+					<div class="col-12 col-lg bg-white p-3">
+						<div class="consultation-card__detail mb-2"><?= $serviceInfo[0]->title;?></div>
+						<div class="consultation-card__pro"><?= $staffName; ?></div>
+						<div class="consultation-card__detail"><?= $staffTrade; ?></div>
+					</div>
+					<div class="col-12 col-lg p-3">
+						<div><?= $date; ?></div>
+						<div><?= date_format($startTime, 'g:i').'-'.date_format($endTime, 'g:i').$timeOfDay; ?></div>
+						<div><a class="text-button text-button--green" href="/pro/<?= str_replace(' ', '-', strtolower($staffInfo[0]->full_name)); ?>/#booking-<?= $serviceId; ?>">Change Time</a></div>
+					</div>
+					<div class="col-12 col-lg p-3 d-flex justify-content-lg-end">
+						<div><?php echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></div>
+					</div>
+				</div>
+			<?php else: ?>
+				<div class="order-item row mb-borders m-0 mb-4">
+					<div class="col-12 col-lg p-3">
+						<div class="order-item__image"><?= $_product->get_image(); ?></div>
+					</div>
+					<div class="col-12 col-lg p-3">
+						<div class="order-item__name product-name mb-3" data-title="<?php esc_attr_e( 'Product', 'woocommerce' ); ?>">
+							<?php
+							if ( ! $product_permalink ) {
+								echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) . '&nbsp;' );
+							} else {
+								echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', sprintf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $_product->get_name() ), $cart_item, $cart_item_key ) );
+							}
+
+							do_action( 'woocommerce_after_cart_item_name', $cart_item, $cart_item_key ); ?>
+						</div>
+
+						<div class="order-item__details">
+							<?= wc_get_formatted_cart_item_data($cart_item); // PHPCS: XSS ok. ?>
+						</div>
+
+						<div class="product-price mb-3" data-title="<?php esc_attr_e( 'Price', 'woocommerce' ); ?>">
+							<?php
+								echo apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key ); // PHPCS: XSS ok.
+							?>
+						</div>
+
+						<div class="product-remove">
+							<?php
+								echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+									'woocommerce_cart_item_remove_link',
+									sprintf(
+										'<a href="%s" class="text-button text-button--green" aria-label="%s" data-product_id="%s" data-product_sku="%s">Remove</a>',
+										esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
+										esc_html__( 'Remove this item', 'woocommerce' ),
+										esc_attr( $product_id ),
+										esc_attr( $_product->get_sku() )
+									),
+									$cart_item_key
+								);
+							?>
+						</div>
+					</div>
+
+					<div class="col-12 col-lg p-3 d-flex justify-content-lg-end">
+						<div class="product-subtotal" data-title="<?php esc_attr_e( 'Subtotal', 'woocommerce' ); ?>">
+							<?php echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); // PHPCS: XSS ok. ?>
+						</div>
+					</div>
+				</div>
+			<?php endif; ?>
+			<?php
+		}
+	}
+
+	do_action( 'woocommerce_review_order_after_cart_contents' );
 ?>
 
 <hr/>
